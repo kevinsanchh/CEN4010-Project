5633f7534a15061decb6b2e5c673bb4b47edfda1
Vi-deve31 <qveentoria@gmail.com>
2024-09-27 16:29:43 -0400

    create mongo database and initialized spring

---
+++ GeekTextProject/src/main/java/com/example/GeekTextProject/GeekTextProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.GeekTextProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekTextProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekTextProjectApplication.class, args);
+	}
+
+}
---
+++ GeekTextProject/src/test/java/com/example/GeekTextProject/GeekTextProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GeekTextProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekTextProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
